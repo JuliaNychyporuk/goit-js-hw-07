1981c27db38bff9153f9072d0d887ef08df23ae9
JuliaNychyporuk <martinenko0516@gmail.com>
2023-01-27 22:27:26 -0500

    Commit-ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+// Change code below this line
+const galleryListContainer = document.querySelector('.gallery')
+
+function creategalleryItemsMarkUp (items) {
+   
+   const galleryIitems = items.reduce((acc, {original, preview, description}) => { 
+    
+    const template = `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    return acc += template}, '')
+    
+    galleryListContainer.innerHTML = galleryIitems
+   
+}
+
+creategalleryItemsMarkUp(galleryItems)
+
+galleryListContainer.addEventListener('click', onFullScreenSizeImageClick)
+
+const instance = basicLightbox.create(
+  `<img width="1400" height="900" src = ''>`, {
+    onShow: instance => {window.addEventListener('keydown', onEscClick)
+  },
+    onClose: instance => {window.addEventListener('keydown', onEscClick)
+  }
+})
+
+function onFullScreenSizeImageClick (evt) {
+  
+  evt.preventDefault()
+
+  if (evt.target.nodeName !== 'IMG') {
+    return
+  }
+
+  instance.element().querySelector('img').src = evt.target.dataset.source;
+
+  instance.show();
+}
+
+function onEscClick (evt) {
+    if (evt.key === 'Escape') {
+    instance.close()
+    return}
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListContainer = document.querySelector('.gallery')
+
+function creategalleryItemsMarkUp (items) {
+   
+   const galleryItems = items.reduce((acc, {original, preview, description}) => { 
+    
+    const template = `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    return acc += template}, '')
+    
+    galleryListContainer.innerHTML = galleryItems
+   
+}
+
+creategalleryItemsMarkUp(galleryItems)
+
+let lightbox = new SimpleLightbox('.gallery__link', {captionDelay: 250, captionsData: 'alt'})
